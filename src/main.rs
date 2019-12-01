e2389400bbfbfa7a1e81bdb35c9e43548ad22b53
Gabriel Cardona <cgcardona@gmail.com>
2019-12-01 15:21:48 -0800

    Get seed and hex string.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
-use keyphrase::{Language, Mnemonic, MnemonicType};
+use keyphrase::{Language, Mnemonic, MnemonicType, Seed};
 
 fn main() {
     better_panic::install();
     let mnemonic = Mnemonic::new(MnemonicType::Words12, Language::Japanese);
     let phrase: &str = mnemonic.phrase();
     println!("phrase: {}", phrase);
+
+    // get the HD wallet seed
+    let seed = Seed::new(&mnemonic, "");
+
+    // get the HD wallet seed as raw bytes
+    let seed_bytes: &[u8] = seed.as_bytes();
+
+    // print the HD wallet seed as a hex string
+    println!("{:X}", seed);
 }
